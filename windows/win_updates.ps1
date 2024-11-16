7bbd20451d6b6688952cedb63c2c491e5681a0db
Pedro Dias <jpedrodias@gmail.com>
2024-11-16 12:10:34 +0000

    Update win_updates.ps1

--- windows/win_updates.ps1
+++ windows/win_updates.ps1
@@ -1,6 +1,6 @@
 
 # 0. Permissões: Certifique-se de executar este script com privilégios de administrador.
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 # Set default action for .ps1 file as open on notepad
 
 # 0. Get-WindowsUpdate: Esse comando faz parte do módulo PowerShell PSWindowsUpdate, que pode não estar instalado por padrão. Pode ser instalado com:
