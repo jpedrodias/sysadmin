bbbf1ddad36478d6c8a3e0c7434124f9bc7a8c85
Pedro Dias <jpedrodias@gmail.com>
2024-11-16 12:01:07 +0000

    Update win_updates.ps1

--- windows/win_updates.ps1
+++ windows/win_updates.ps1
@@ -1,15 +1,16 @@
 
 # 0. Permissões: Certifique-se de executar este script com privilégios de administrador.
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 # Set default action for .ps1 file as open on notepad
 
-
 # 0. Get-WindowsUpdate: Esse comando faz parte do módulo PowerShell PSWindowsUpdate, que pode não estar instalado por padrão. Pode ser instalado com:
+Write-Host "Instalando PSWindowsUpdate..."
 Install-Module -Name PSWindowsUpdate -Force -AllowClobber
 
 # 1. Atualizar todos os pacotes via Winget
+Write-Host "winget upgrade (all) ..."
 winget upgrade --all --include-unknown --accept-package-agreements --accept-source-agreements
 
 # 2. Executar atualizações do Windows Update
-Write-Host "Procurando por atualizações do Windows..."
+Write-Host "Fazendo atualizações do Windows..."
 Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot
